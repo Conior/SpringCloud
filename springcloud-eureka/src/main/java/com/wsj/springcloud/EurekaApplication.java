145663f77d39a59e84d4327c2ef30b900939c207
王施鉴 <wsj249565@gongdao.com>
2021-01-08 09:05:12 +0800

    Eureka注册中心

---
+++ springcloud-eureka/src/main/java/com/wsj/springcloud/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.wsj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wangshijian
+ * @Description:
+ * @date 2021/1/7 11:02 下午
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
